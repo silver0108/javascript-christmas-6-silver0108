3e2f0470163b36d20f20ee832306e174e183f128
silver0108 <kk1170@naver.com>
2023-11-14 00:15:37 +0900

    refactor: 입력 상수 메시지 출력

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,10 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 import Vaildator from "../vaildator/vaildator.js";
+import { INPUT_MESSAGE } from "../constants/constants.js";
 
 const InputView = {
     async readDate() {
         try {
-            const date = Number(await Console.readLineAsync());
+            const date = Number(await Console.readLineAsync(INPUT_MESSAGE.inputDate));
             
             Vaildator.isNumber(date); // 숫자인지
             Vaildator.isNumberInRange(date); // 1 ~ 31 범위인지
