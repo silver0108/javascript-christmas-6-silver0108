d460ea6facfaf27ba81f233d0a15140dee362b23
silver0108 <kk1170@naver.com>
2023-11-13 22:44:48 +0900

    feat: 식당 방문 날짜 입력

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,7 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 const InputView = {
     async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
+        const date = await MissionUtils.Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        
+        return date;
     }
-    // ...
 }
+
+export default InputView;
