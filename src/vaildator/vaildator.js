e553051a188c43317e0c038812e8302ea4f66f06
silver0108 <kk1170@naver.com>
2023-11-13 23:40:54 +0900

    feat: 에러 메시지 상수 설정

--- src/vaildator/vaildator.js
+++ src/vaildator/vaildator.js
@@ -1,15 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
+import { ERROR_MESSAGE } from "../constants/error.js";
 
 class Vaildator {
   static isNumber(num) {
     if (!Number.isInteger(num)) {
-      throw new Error("[ERROR]");
+      throw new Error(ERROR_MESSAGE.notValidDate);
     }
   }
 
   static isNumberInRange(num) {
     if(num < 1 || num > 31){
-      throw new Error("[ERROR] 1~31");
+      throw new Error(ERROR_MESSAGE.notValidDate);
     }
   }
 }
