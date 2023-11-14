c9d9650cce0ff828c6e67944a775cd0b415e138a
silver0108 <kk1170@naver.com>
2023-11-14 20:49:19 +0900

    feat: 평일 할인 계산

--- src/models/Calculator.js
+++ src/models/Calculator.js
@@ -1,21 +1,25 @@
 import { Console } from "@woowacourse/mission-utils";
+import isWeekend from "../utils/IsWeekend.js";
 
 class Calculator {
   #orders;
   #date;
   #toalQuantity;
-  #totalPrice;
+  #totalPrice=0;
+  #weekdayDiscount=0;
 
   constructor(orders, date) {
     this.#orders = orders;
     this.#date = date;
+    this.calculateTotalPrice();
+    this.calculateGiftMenu();
+    this.calculateBenefit();
   }
 
   calculateTotalPrice() {
-    const totalPrice = this.#orders.reduce((total, order) => 
+    this.#totalPrice = this.#orders.reduce((total, order) => 
       total + Number(order.getOrderMenu().getPrice() * order.getMenuQuantity()), 0);
-    
-    return totalPrice;
+    return this.#totalPrice;
   }
 
   calculateGiftMenu() {
@@ -25,9 +29,29 @@ class Calculator {
     return false;
   }
 
+  calculateBenefit() {
+    if(this.#totalPrice >= 10000){
+      this.calculateChristmasDday();
+      this.calculateWeekday();
+    }
+  }
+
   calculateChristmasDday() {
     if(this.#date >= 1 && this.#date <=25) {
       return -(1000 + (this.#date - 1) * 100); 
+    }
+    return false;
+  }
+
+  calculateWeekday() {
+    if(!isWeekend(this.#date)) {
+      for(const order of this.#orders) {
+        const menu = order.getOrderMenu();
+        if(menu.getCategory() === "DESSERT") {
+          this.#weekdayDiscount += 2023 * order.getMenuQuantity(); 
+          return -this.#weekdayDiscount;
+        }
+      }
     }
     return false;
   }
---
+++ src/utils/IsWeekend.js
@@ -0,0 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const isWeekend = (date) => {
+  const currentYear = new Date().getFullYear();
+  const inputDate = new Date(currentYear, 11, date); // 12월은 11을 나타냄
+  const day = inputDate.getDay();
+
+  return day === 5 || day === 6;
+}
+
+export default isWeekend;
