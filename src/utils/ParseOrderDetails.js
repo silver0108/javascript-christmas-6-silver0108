153c6e36a63350b27360f68ab8365b84a02ab29a
silver0108 <kk1170@naver.com>
2023-11-14 11:05:50 +0900

    refactor: 주문 수량 변수 이름 변경

--- src/utils/ParseOrderDetails.js
+++ src/utils/ParseOrderDetails.js
@@ -1,7 +1,7 @@
 const parseOrderDetails = (orderDetails) => {
   const menus = orderDetails.trim().split(',').map(item => {
-    const [name, count] = item.trim().split('-');
-    return { menuName: name, menuCnt: count };
+    const [name, quantity] = item.trim().split('-');
+    return { menuName: name, quantity: quantity };
   })
   return menus;
 }
